d02655c2ebf925841e216134f1fd8cb4bf23e6f6
Jeffrey Lott <Jeffrey.M.Lott@gmail.com>
2019-09-04 19:59:33 -0700

    Creating a console application

---
+++ Mg.Challenge.App/Program.cs
@@ -0,0 +1,45 @@
+﻿using Mg.Challenge.Core.Services;
+using System;
+
+namespace Mg.Challenge.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileAccessor = new FileAccessor();
+            var parsingService = new FileParsingService();
+            var writer = new JsonFileWriter();
+
+            Console.WriteLine("Please enter the full path to the CSV file:");
+            var inputPath = Console.ReadLine();
+            Console.WriteLine("Please enter the output path for the JSON file:");
+            var outputPath = Console.ReadLine();
+
+            var lines = fileAccessor.ReadFile(inputPath);
+
+            if (lines == null)
+            {
+                Console.WriteLine($"Was not able to read {inputPath}");
+                return;
+            }
+
+            var dto = parsingService.Parse(lines);
+
+            if(dto == null)
+            {
+                Console.WriteLine($"Error parsing file: {inputPath}");
+                return;
+            }
+
+            var wasSuccessful = writer.WriteFileAsJson(outputPath, dto);
+
+            if (wasSuccessful)
+                Console.WriteLine($"Successfully wrote the JSON to {outputPath}");
+            else
+                Console.WriteLine($"Failed to write the JSON to {outputPath}");
+
+            Console.ReadLine();
+        }
+    }
+}
