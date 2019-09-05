08debccc2a0c581dadf672357034e71ce0aa9772
Jeffrey Lott <Jeffrey.M.Lott@gmail.com>
2019-09-04 19:33:04 -0700

    Adding other services.

---
+++ mg.challenge.core/Services/FileAccessor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Mg.Challenge.Core.Services
+{
+    public class FileAccessor
+    {
+        public string[] ReadFile(string fileName)
+        {
+            if (File.Exists(fileName))
+                return File.ReadAllLines(fileName);
+
+            return null;
+        }
+    }
+}
---
+++ mg.challenge.core/Services/JsonFileWriter.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Mg.Challenge.Core.Services
+{
+    public class JsonFileWriter
+    {
+        public bool WriteFileAsJson<T>(string fileName, T item)
+        {
+            if (!File.Exists(fileName))
+            {
+                try
+                {
+                    File.WriteAllText(fileName, JsonConvert.SerializeObject(item));
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+
+            return false;
+        }
+    }
+}
